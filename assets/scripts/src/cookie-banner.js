0249142dd0f29267d00745df33502040d234acee
Erik Bernskiold <erik@bernskioldmedia.com>
2019-09-09 17:19:28 +0200

    Update styling & prod build

--- assets/scripts/src/cookie-banner.js
+++ assets/scripts/src/cookie-banner.js
@@ -2,10 +2,10 @@
 
 /**
  * Ilmenite Cookie Consent
- * 
+ *
  * @author Bernskiold Media <info@bernskioldmedia.com>
  */
-let Ilmenite_Cookie_Consent = function($, ilcc) {
+const Ilmenite_Cookie_Consent = function( $, ilcc ) {
 
 	/**
 	 * Module Definition
@@ -20,7 +20,7 @@ let Ilmenite_Cookie_Consent = function($, ilcc) {
 		settings: {
 			noCookieMode: false, // Debug Mode: true will disable the cookie, allowing you to debug the banner.
 			consentRememberDuration: 30, // Duration in Days: The number of days before the cookie should expire.
-			cookieName: 'EUCookieConsent', // The name of the cookie.
+			cookieName: "EUCookieConsent", // The name of the cookie.
 			cookieActiveValue: 1 // The active value of the cookie.
 		},
 
@@ -34,10 +34,10 @@ let Ilmenite_Cookie_Consent = function($, ilcc) {
 
 				// Set the consent duration into a cookie date string
 				var date = new Date();
-				date.setTime(date.getTime()+(module.settings.consentDuration*24*60*60*1000));
+				date.setTime( date.getTime() + ( module.settings.consentDuration * 24 * 60 * 60 * 1000 ) );
 
 				// Set the actual cookie
-				document.cookie = module.settings.cookieName + '=' + module.settings.cookieActiveValue + '; expires=' + date.toGMTString() + '; path=/';
+				document.cookie = module.settings.cookieName + "=" + module.settings.cookieActiveValue + "; expires=" + date.toGMTString() + "; path=/";
 
 			}
 
@@ -50,35 +50,40 @@ let Ilmenite_Cookie_Consent = function($, ilcc) {
 		createBanner: function() {
 
 			// Set the contents.
-			const consentBlock = '<div class="ilcc-cookie-consent-notice js--ilcc-cookie-consent-notice" id="cookie-consent-block"><p><span>' + ilcc.cookieConsentTitle + '</span>' + ilcc.cookieConsentText + '<button class="ilcc-cookie-consent-close js--ilcc-cookie-consent-close close-cookie-block">' + ilcc.acceptText + '</button></p></div>';
+			const consentBlock = "<div class=\"ilcc-cookie-consent-notice js--ilcc-cookie-consent-notice\" id=\"cookie-consent-block\"><div class=\"ilcc-cookie-consent-notice-content\"><p><span>" + ilcc.cookieConsentTitle + "</span>" + ilcc.cookieConsentText + "</p><button class=\"ilcc-cookie-consent-close js--ilcc-cookie-consent-close close-cookie-block\">" + ilcc.acceptText + "</button></div></div>";
 
 			// Get body tag
-			const $body = $('body');
+			const $body = $( "body.has-ilcc-banner" );
 
 			// Append to body
-			$body.append(consentBlock);
+			$body.append( consentBlock );
 
 			// Get the height of the consent block
-			var consentBlockHeight = $('.js--ilcc-cookie-consent-notice').innerHeight();
+			const consentBlockHeight = $( ".js--ilcc-cookie-consent-notice" ).innerHeight();
 
 			// Add class to body
-			$body.addClass('has-cookie-banner');
-			$body.css('padding-top', consentBlockHeight + 'px');
+			if ( $body.hasClass( "ilcc-style-top" ) ) {
+				$body.css( "padding-top", consentBlockHeight + "px" );
+			}
 
 		},
 
 		/**
 		 * Get the value of a cookie by its name.
-		 * 
-		 * @param {string} name 
+		 *
+		 * @param {string} name
 		 */
-		getCookieValue: function(name) {
+		getCookieValue: function( name ) {
 			let nameEQ = name + "=";
-			let ca = document.cookie.split(';');
-			for(let i=0;i < ca.length;i++) {
-				let c = ca[i];
-				while (c.charAt(0)==' ') c = c.substring(1,c.length);
-				if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+			let ca     = document.cookie.split( ";" );
+			for ( let i = 0; i < ca.length; i++ ) {
+				let c = ca[ i ];
+				while ( c.charAt( 0 ) == " " ) {
+					c = c.substring( 1, c.length );
+				}
+				if ( c.indexOf( nameEQ ) == 0 ) {
+					return c.substring( nameEQ.length, c.length );
+				}
 			}
 			return null;
 		},
@@ -87,24 +92,32 @@ let Ilmenite_Cookie_Consent = function($, ilcc) {
 		 * Remove the cookie banner from the page.
 		 */
 		removeBanner: function() {
-			$('.js--ilcc-cookie-consent-notice').slideToggle({
+			$( ".js--ilcc-cookie-consent-notice" ).slideToggle( {
 				start: function() {
 
-					$('body').animate({
-						'padding-top': '0px'
-					});
+					let $body = $( "body" );
+
+					if ( $body.hasClass( "ilcc-style-top" ) ) {
+						$body.animate( {
+							"padding-top": "0px"
+						} );
+					}
 
 				},
 				complete: function() {
 
 					// Remove cookie banner class
-					$('body').removeClass('has-cookie-banner');
-					
+					$( "body" )
+						.removeClass( "has-ilcc-banner" )
+						.removeClass( "ilcc-style-top" )
+						.removeClass( "ilcc-style-overlay" )
+						.addClass( "has-ilcc-consented" );
+
 					// Remove the cookie banner from the DOM.
-					$(this).remove();
-	
+					$( this ).remove();
+
 				}
-			});
+			} );
 		}
 
 	};
@@ -114,22 +127,22 @@ let Ilmenite_Cookie_Consent = function($, ilcc) {
 	 * the cookie terms (ie. we have no cookie), then we
 	 * create the banner.
 	 */
-	$(window).load(function() {
-		if(module.getCookieValue(module.settings.cookieName) != module.settings.cookieActiveValue ) {
+	$( window ).load( function() {
+		if ( module.getCookieValue( module.settings.cookieName ) != module.settings.cookieActiveValue ) {
 			module.createBanner();
 		}
-	});
+	} );
 
 	/**
 	 * If the user clicks on the accept button to close the banner,
 	 * we remove it and set the accepted cookie.
 	 */
-	$(document.body).on('click', '.js--ilcc-cookie-consent-close', function() {
+	$( document.body ).on( "click", ".js--ilcc-cookie-consent-close", function() {
 		module.removeBanner();
 		module.setCookie();
-	});
+	} );
 
 	// Return the module.
 	return module;
 
-}(jQuery, ilcc);
+}( jQuery, ilcc );
